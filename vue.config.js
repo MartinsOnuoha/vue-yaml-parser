1ecd57d290eafe3af2ccd3398e52fc58d301639f
MartinsOnuoha <martinsvictor.nd@gmail.com>
2021-08-25 08:29:47 +0100

    chore(components): optimize child component imports

--- vue.config.js
+++ vue.config.js
@@ -10,4 +10,10 @@ module.exports = {
       ],
     },
   },
+  chainWebpack: (config) => {
+    config.plugin('copy').tap((options) => {
+      options[0][0].ignore.push('public/demo.gif');
+      return options;
+    });
+  },
 };
