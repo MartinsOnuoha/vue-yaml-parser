534feaeb098568c6c2611aa11fef163e6a880d93
MartinsOnuoha <martinsvictor.nd@gmail.com>
2021-08-26 03:24:31 +0100

    feat(UploadBtn): adds upload button feat

--- src/components/content/Itenary/Itenary.vue
+++ src/components/content/Itenary/Itenary.vue
@@ -4,8 +4,10 @@
       <h3 class="text-primary font-semibold">
         <font-awesome-icon class="mr-2" icon="route" :style="{ color: '#a3c6c9' }" />
         Itienary
+      <upload-btn @selected="setFile" />
       </h3>
     </div>
+
     <div class="flex items-center flex-col">
       <div class="items" v-for="({ images, title, description }, index) in itenaries" :key="index">
         <item
@@ -23,13 +25,14 @@
   .itenary {
     .header-text {
       margin-bottom: 10px;
-      margin-left: -45rem;
+      margin-left: -35rem;
       font-size: 20px;
     }
   }
 </style>
 <script>
-import ItenaryData from '@/assets/data.yaml';
+import YAML from 'yaml';
+import { parseToText } from '@/utils/helpers';
 
 const staticImages = [
   { src: 'https://picsum.photos/seed/picsum/450/300' },
@@ -42,11 +45,26 @@ export default {
   name: 'Itenary',
   components: {
     Item: () => import('@/components/reusables/Item/Item.vue'),
+    UploadBtn: () => import('@/components/reusables/UploadBtn/UploadBtn.vue'),
+  },
+  data() {
+    return {
+      daysData: [],
+    };
   },
   computed: {
     itenaries() {
-      return ItenaryData.days.map((day) => ({ images: staticImages, ...day }));
+      return this.daysData.map((day) => ({ images: staticImages, ...day }));
     },
+  },
+  methods: {
+    async setFile(file) {
+      const result = await parseToText(file);
+      const jsonResult = await YAML.parse(result);
+      this.daysData = jsonResult.days;
+    },
+  },
+  mounted() {
   },
 };
 </script>
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,13 @@
 const truncate = (text, maxLength) => (text.length > maxLength ? `${text.substr(0, maxLength)}...` : text);
 
+// let text = ''
+const parseToText = async (fileBlob) => new Promise((resolve) => {
+  const reader = new FileReader();
+  reader.onloadend = () => resolve(reader.result);
+  reader.readAsText(fileBlob);
+});
+
 export {
-  // eslint-disable-next-line import/prefer-default-export
+  parseToText,
   truncate,
 };
